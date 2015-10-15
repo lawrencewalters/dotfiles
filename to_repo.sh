f07c922d03055328dc81b6b41674ae46d2f254d6
Lawrence Walters <lawrencewalters@gmail.com>
2015-10-15 17:25:42 -0500

    get hidden dot files

--- to_repo.sh
+++ to_repo.sh
@@ -7,21 +7,23 @@ function my_unixpath() {
 }
 
 # look through folders in /src
+# for each dir with a matching local var set by msysgit(USERPROFILE)
+#TODO: capture hidden files
 for d in src/*/ ; do
 	path_var=${d%/}
 	path_var=${path_var#src/}
 	expanded_path=${!path_var}
 	unix_path=$(my_unixpath "$expanded_path")
 	echo "src=$path_var windows=$expanded_path unix=$unix_path"
-	for f in "$d"* ; do
+	# find the local system file that matches
+	# for each file in the repo
+	for f in "$d"* "$d".[^.]*; do
+		# TODO: recurse into directories here!
 		echo "  ${f##*/}"
-		echo "    cp $unix_path/${f##*/} src/$path_var/${f##*/}"
+		echo "    cp \"$unix_path/${f##*/}\" \"src/$path_var/${f##*/}\""
+		# overwrite the repo file with the local system file	
 		eval "cp \"$unix_path/${f##*/}\" \"src/$path_var/${f##*/}\""
 	done
 done
-# for each dir with a matching local var set by msysgit(USERPROFILE)
-# for each file in the repo
-# find the local system file that matches
-# overwrite the repo file with the local system file	
 
 
