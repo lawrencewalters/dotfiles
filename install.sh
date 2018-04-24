b89ad2220382d0008fe50e4cae5a3b5711274f47
Lawrence Walters <lwalters@lyonscg.com>
2018-04-24 12:36:06 -0500

    getting conemu to right place

--- install.sh
+++ install.sh
@@ -38,6 +38,7 @@ for f in ConEmu.xml; do
 done
 
 cautious_link "${HOME}/dotfiles/AppData/Roaming/Code/User/settings.json" "${HOME}/AppData/Roaming/Code/User/settings.json"
+cautious_link "${HOME}/dotfiles/AppData/Roaming/ConEmu.xml" "${HOME}/AppData/Roaming/ConEmu.xml"
 
 #TODO: keypirinha, C:\Users\lwalters\AppData\Roaming\Keypirinha\User, C:\Users\lwalters\AppData\Roaming\Notepad++
 #for x in ./bin/*; do
