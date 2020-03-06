b47cba21081ec14de04aec4d41bb56ee52d78bb2
Lawrence Walters <lwalters@lyonscg.com>
2020-03-06 12:18:39 +0100

    minor cleanup

--- shell_profile/smartnavrestart.ps1
+++
@@ -1,7 +0,0 @@
-# script to restart pesky SmartNAV with powershell
-# I find that smartnav is always dying on me in windows 8, so I'm constantly restarting it.
-# create a shortcut to this, like C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe "& "C:\foo\bar\smartnavrestart.ps1"
-# mark the shortcut to run as admin
-Get-Process | Where-Object { $_.Name -eq "SmartNAV" } | Select-Object -First 1 | Stop-Process
-#Read-Host -Prompt "Press Enter to continue"
-Start-Process "C:\Program Files (x86)\NaturalPoint\SmartNav\SmartNAV.exe"
