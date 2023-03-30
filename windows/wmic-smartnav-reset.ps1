a6c00229dcfe5336b61fd6d8bb5dff7e251bd389
Lawrence Walters <lawrencewalters@users.noreply.github.com>
2023-03-30 13:56:27 -0400

    update execution notes

--- windows/wmic-smartnav-reset.ps1
+++ windows/wmic-smartnav-reset.ps1
@@ -1,6 +1,7 @@
-# script to restart pesky SmartNAV with powershell, and Cap's stupid policy prevents the other version from running as admin
+# script to restart pesky SmartNAV with powershell, and sometimes stupid policy prevents the other version from running as admin
 # this one should not require admin rights
-# I find that smartnav is always dying on me in windows 8, so I'm constantly restarting it.
-# create a shortcut to this, like C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe "& "C:\foo\bar\thisfile.ps1"
+# I find that smartnav is always dying on me in windows 10, so I'm constantly restarting it, usually
+# after machine sleeps
+# create a shortcut to this, like C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -Command "C:\foo\bar\thisfile.ps1"
 wmic process where "name='SmartNAV.exe'" delete
 Start-Process "C:\Program Files (x86)\NaturalPoint\SmartNav\SmartNAV.exe"
